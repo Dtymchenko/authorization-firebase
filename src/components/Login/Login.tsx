26e869af9c27693380a451e7f5114e121169cd0a
Dmytro Tymchenko <timchenkod88@gmail.com>
2023-03-04 02:12:13 +0200

    done reg and sign in

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Login.module.scss'
+import { useAppDispatch } from '../../hooks/redux-hooks'
+import Form from '../Form/Form'
+import { setUser } from '../../store/slices/userSlice'
+import { getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+
+    const handleLogin = (email:string, password:string) => {
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, email, password)
+        .then(({user}) => {
+          dispatch(setUser({
+              email:user.email,
+              id: user.uid,
+              token: user.refreshToken
+          }))
+          navigate('/')
+      })
+            .catch(() => alert('Wrong username or password'))
+            
+    }
+
+  return (
+    <Form
+    title='sign in'
+    handleClick={handleLogin}/>
+  )
+}
+
+export default Login
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './SignUp.module.scss'
+import { useAppDispatch } from '../../hooks/redux-hooks'
+import Form from '../Form/Form'
+import { setUser } from '../../store/slices/userSlice'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom'
+
+const SignUp = () => {
+
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+
+    const handleRegister = (email:string, password:string) => {
+        const auth = getAuth()
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(({user}) => {
+                dispatch(setUser({
+                    email:user.email,
+                    id: user.uid,
+                    token: user.refreshToken
+                }))
+                navigate('/')
+            })
+            .catch(console.error)
+    }
+
+  return (
+    <Form
+    title='register'
+    handleClick={handleRegister}/>
+  )
+}
+
+export default SignUp
